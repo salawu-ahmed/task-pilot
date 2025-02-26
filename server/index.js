0b74b8aac90f89d6cc9c37d89f2d854705f63974
ahmed <a.salawu@outlook.com>
2025-02-26 22:54:11 +0000

    added registration and login routes

--- server/index.js
+++ server/index.js
@@ -3,23 +3,20 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
 require('dotenv').config()
+const dbConnect = require('./config/db')
 const app = express()
 
 // routes
-const userRoutes = require('./routes/UserRoutes')
+const userRoutes = require('./routes/user.routes')
+const  authRoutes  = require('./routes/auth.routes')
 
 // middlewares
 app.use(bodyParser.json())
 app.use(cors())
+app.use('/auth', authRoutes)
 app.use('/users', userRoutes)
 
 // database connnection
-mongoose.connect('mongodb://127.0.0.1:27017/taskPilot')
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-
-db.once('open', () => {
-  console.log('Connected to MongoDB');
-});
+dbConnect()
 
 app.listen(process.env.port, console.log(`Server is running on port ${process.env.port}`))
