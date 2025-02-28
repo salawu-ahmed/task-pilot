c79847217c2b6076aa0aba4d1b506c65ec1946f5
ahmed <a.salawu@outlook.com>
2025-02-28 18:33:25 +0000

    styled signup components

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router'
 
 function Signup() {
   // const API_BASEURL = process.env.REACT_APP_API_BASEURL
-  const API_BASEURL = import.meta.env.VITE_REACT_APP_API_BASEURL 
+  const API_BASEURL = import.meta.env.VITE_REACT_APP_API_BASEURL
 
   const [formData, setFormData] = useState({
     userFirstName: '',
@@ -23,17 +23,17 @@ function Signup() {
       userPassword: formData.userPassword,
     }
     console.log(data);
-    
+
     axios.post(`${API_BASEURL}/users/registerUser`, data)
-    .then(res => console.log(res.data))
+      .then(res => console.log(res.data))
     // .then(data => console.log(data))
   }
 
   return (
-    <div>
-      <h1>Welcome to TaskPilot</h1>
-      <form onSubmit={handleSubmit}>
-        <div className='flex'>
+    <div className='container w-full h-screen flex items-center flex-col justify-center bg-gray-200'>
+      <div className='md:w-64'>
+        <h1 className='text-3xl md:text-5xl mb-4 text-center font-bold'>Welcome</h1>
+        <form onSubmit={handleSubmit} className='flex space-y-4 flex-col mb-4'>
           <div>
             <input
               type="text"
@@ -45,6 +45,7 @@ function Signup() {
                   [e.target.name]: e.target.value
                 }
               })}
+              className='border focus:outline-none rounded-md py-2 px-6 block'
             />
           </div>
           <div>
@@ -58,40 +59,50 @@ function Signup() {
                   [e.target.name]: e.target.value
                 }
               })}
+              className='border focus:outline-none rounded-md py-2 px-6 block'
             />
           </div>
-        </div>
-        <div>
-          <input
-            type="email"
-            name='userEmail'
-            placeholder='Enter your email'
-            onChange={(e) => setFormData((prev) => {
-              return {
-                ...prev,
-                [e.target.name]: e.target.value
-              }
-            })}
-          />
-        </div>
-        <div>
-          <input
-            type="password"
-            name="userPassword"
-            id=""
-            placeholder='Enter a strong password'
-            onChange={(e) => setFormData((prev) => {
-              return {
-                ...prev,
-                [e.target.name]: e.target.value
-              }
-            })}
-          />
-        </div>
-        <button type='submit'>Create account</button>
-      </form>
-      <button onClick={handleSubmit}>Sign up with Google</button>
-      <p>Already have an account? <Link to='/login'>Login</Link></p>
+          <div>
+            <input
+              type="email"
+              name='userEmail'
+              placeholder='Enter your email'
+              onChange={(e) => setFormData((prev) => {
+                return {
+                  ...prev,
+                  [e.target.name]: e.target.value
+                }
+              })}
+              className='border focus:outline-none rounded-md py-2 px-6 block'
+            />
+          </div>
+          <div>
+            <input
+              type="password"
+              name="userPassword"
+              id=""
+              placeholder='Enter a strong password'
+              onChange={(e) => setFormData((prev) => {
+                return {
+                  ...prev,
+                  [e.target.name]: e.target.value
+                }
+              })}
+              className='border focus:outline-none rounded-md py-2 px-6 block'
+            />
+          </div>
+          <button
+            type='submit'
+            className='w-full bg-gray-700 text-white hover:bg-gray-900 py-2 rounded-md'
+          >Create account</button>
+        </form>
+        <p className='relative overflow-hidden text-center w-full text-gray-950 mb-4 text-xl
+                before:w-1/2 before:h-0.5 before:block before:bg-gray-400 before:absolute before:top-1/2 before:transform before:-translate-x-4.5
+                after:w-1/2 after:h-0.5 after:block after:bg-gray-400 after:absolute after:top-1/2 after:transform after:translate-x-full after:ml-4.5 '
+        >or</p>
+        <button onClick={handleSubmit} className='w-full px-6 py-2 rounded-md bg-gray-900 hover:bg-gray-950 text-white mb-2'>Sign up with Google</button>
+        <p>Already have an account? <Link to='/login' className='text-blue-500'>Login</Link></p>
+      </div>
     </div>
   )
 }
